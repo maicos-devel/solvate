e25f9fd4f80d20d2729064decdda6b33eced1329
hejamu <info@jaegerhenrik.de>
2022-05-19 11:50:46 +0200

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name="nonmaicos",
   version="0.0.1",
-  packages=["nonmaicos"]
+  packages=["nonmaicos"],
   install_requires=[
    'MDAnalysis>=2.0.0',
    'numpy'
