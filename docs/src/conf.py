34e82dabd0cfbd3a3a8d780d261b13fd2688b95c
hejamu <info@jaegerhenrik.de>
2026-05-20 15:36:40 +0200

    Add more info to the API

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -76,7 +76,7 @@ autodoc_default_options = {
     "members": True,
     "show-inheritance": True,
 }
-autodoc_typehints = "description"
+autodoc_typehints = "none"
 
 # -- Options for HTML output -------------------------------------------------
 
